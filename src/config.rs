eb6e7d89256c40f0055249f27cb6b69c98672bc7
ifantasyw <ifantasyw@gmail.com>
2025-07-21 16:19:49 +0800

    ID/Server

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["8.149.138.20"];
+pub const RS_PUB_KEY: &str = "Ev5q6UCTxp2xMGbg2vC4bEEvfRcUOD1JHcAybdVSrOo=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
